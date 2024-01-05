ebd90f71de8734520c5facddc18de4a90f983694
jidinen <gofwenjidinen05@gmail.com>
2024-01-05 07:41:15 +0100

    made some scripts

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+
+def main():
+    """Making a function that takes command line argumets
+    Args:
+    no
+
+
+
+    """
+    import sys
+    a = 1
+    firag = sys.argv[0]
+    restag = sys.argv[1:]
+    total = len(sys.argv) - 1
+    if (total <= 0):
+        print("{} argument".format(total))
+
+    elif(total == 1):
+        print("{} argument".format(total))
+    else:
+        print("{} arguments".format(total))
+        for i, arg in enumerate(restag):
+            print("{}: {}".format(a, arg))
+            a += 1
+
+if __name__ == "__main__":
+    main()
