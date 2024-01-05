4413f428d762f4d09241b20d94cbfa0b8b2cea02
jidinen <gofwenjidinen05@gmail.com>
2024-01-05 07:49:10 +0100

    made some adjustment

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,7 +3,7 @@ def main():
     from add_0 import add
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b , add(a,b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
 
 
 if __name__ == "__main__":
--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,10 +3,10 @@ def main():
     import calculator_1
     a = 10 
     b = 5
-    print("{} + {} = {}".format(a,b, calculator_1.add(a,b)))
-    print("{} - {} = {}".format(a,b, calculator_1.sub(a,b)))
-    print("{} * {} = {}".format(a,b, calculator_1.mul(a,b)))
-    print("{} / {} = {}".format(a,b, calculator_1.div(a,b)))
+    print("{:d} + {:d} = {:d}".format(a,b, calculator_1.add(a,b)))
+    print("{:d} - {:d} = {:d}".format(a,b, calculator_1.sub(a,b)))
+    print("{:d} * {:d} = {:d}".format(a,b, calculator_1.mul(a,b)))
+    print("{:d} / {:d} = {:d}".format(a,b, calculator_1.div(a,b)))
 
 
 
