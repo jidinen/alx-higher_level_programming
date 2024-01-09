99f4aece0b1e3eb3f380728bc0de827aa85abe4b
jidinen <gofwenjidinen05@gmail.com>
2024-01-10 00:02:19 +0100

    siging out

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-def element_at(my_list, idx):
+def element_at(my_list: list, idx: int) -> int:
 
     """ deletes an element from a list"""
     if idx < 0 or idx > len(my_list):
--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
 
-def replace_in_list(my_list, idx, element):
+def replace_in_list(my_list: list, idx: int, element: int) -> list:
 
     """ replace and element at certain index"""
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return my_list
     my_list[idx] = element
     return my_list
