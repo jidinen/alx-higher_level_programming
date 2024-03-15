4d0836404b5b0ed9073ab1a16fa72e464643e458
jidinen <gofwenjidinen05@gmail.com>
2024-03-14 21:11:51 -0700

    playiing with databases

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,4 @@
+-- a simple script to show the data base
+-- this is SHOW DATABASE;
+
+SHOW DATABASES;
---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- creating a new database
+-- if  esits it should not fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
