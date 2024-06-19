de353101d6a2b18c8f71b9a0cc69c0bc8ba81980
jidinen <gofwenjidinen05@gmail.com>
2024-06-19 22:12:06 +0100

    made a script using the process.argv function

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,7 +5,7 @@ const myArg = process.argv.slice(2);
 if (myArg.length == 0){
 console.log("No argument");
 }
-else if(myArg.length != 0)
+else if(myArg.length == 1)
 {
 console.log("Argument found");
 }
