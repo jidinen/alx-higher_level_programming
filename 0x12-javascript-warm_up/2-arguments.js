effc7d02e8ea647e44f26fe5470365adca01f190
jidinen <gofwenjidinen05@gmail.com>
2024-06-21 22:37:25 +0100

    trying to fix bugs

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,7 @@
 //variable stores the argument vector of the function
 const myArg = process.argv.slice(2);
 //conditional statement
+
 if (myArg.length == 0){
  console.log('No argument');
 } else if(myArg.length == 1){
