cec7ad8d939da100182c62358b72d584a3efd43e
jidinen <gofwenjidinen05@gmail.com>
2024-06-21 01:54:02 +0100

    used a loop in the script

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-let myArray = ["C is fun", "Python is cool", "Javascript is aweasome"];
+let myArray = ["C is fun", "Python is cool", "Javascript is amazing"];
 for (let i = 0; i < myArray.length; i++)
 {
 console.log(myArray[i]);
