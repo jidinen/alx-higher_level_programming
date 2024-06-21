cc1d15dfd7d5f48fda0a3cc6516b5b4be5687dac
jidinen <gofwenjidinen05@gmail.com>
2024-06-21 02:33:03 +0100

    made a script that willmake square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const myArg = process.argv.slice(2);
+let num = Number(myArg[0]);
+for (let i = 0; i < num; i++)
+{
+let result = [];
+for (let j = 0; j < num; j++){
+let sign = "X";
+result.push(sign)
+}
+let m = result.join("");
+console.log(m);
+}
