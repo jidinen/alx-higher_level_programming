dfa19f98c062c4ed8ad5aa9056fc83750b239017
jidinen <gofwenjidinen05@gmail.com>
2024-06-21 01:42:41 +0100

    still fixing bugs

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
 const myArg = process.argv.slice(2);
-const num = isNaN(myArg[0]);
-if (typeof num === 'number'){
+const num = myArg[0];
+if(!isNaN(num)){
 console.log("My number: " +  num);
+
 }
 else {
 console.log("Not a number");
