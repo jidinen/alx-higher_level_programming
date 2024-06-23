36c91ce18136eefc174500fdbd9af5efa74aedc0
jidinen <gofwenjidinen05@gmail.com>
2024-06-23 23:09:23 +0100

    writing a filter script

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+ writing a Mysqldb script
+
+"""
+import MySQLdb
+import sys
+"""
+importing Mysqldb module and sys function as an argumeent vector
+"""
+
+def fiLter():
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],
+                         port=3306)
+
+    cux = db.cursor()
+    cux.execute("SELECT id, name FROM states")
+
+    states = cux.fetchall()
+
+    for i in states:
+#checking for the first letter 
+        if (states[0] == "N"):
+        print(i)
+   
+
+    db.close()
+
+
+if __name__ == "__main__":
+    fiLter
